559db867416fa105a220d183c8d4af966d688bd2
Giovanni Valente <giovannivalente237@gmail.com>
2026-05-13 20:02:41 -0300

    refatoracao e javadoc

--- src/calculadora/TesteCalculadora.java
+++ src/calculadora/TesteCalculadora.java
@@ -6,11 +6,21 @@ public class TesteCalculadora {
         Calculadora calc = new Calculadora();
 
         // Testes rápidos
-        calc.calc(2, 3, "+"); // esperado: 5
-        calc.calc(10, 4, "-"); // esperado: 6
-        calc.calc(3, 5, "*"); // esperado: 15
-        calc.calc(8, 2, "/"); // esperado: 4
-        calc.calc(8, 0, "/"); // divisão por zero
-        calc.calc(5, 5, "x"); // operação inválida
+        System.out.println(calc.calc(2, 3, "+"));
+        System.out.println(calc.calc(10, 4, "-"));
+        System.out.println(calc.calc(3, 5, "*"));
+        System.out.println(calc.calc(8, 2, "/"));
+
+        try {
+            System.out.println(calc.calc(8, 0, "/")); // exceção
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(calc.calc(5, 5, "x")); // exceção
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
